51a0d1a4141ac0ca48f17adae1aad5108542b7e0
Marcus Quintela <marcus.quintela@gmail.com>
2021-03-27 16:28:48 -0300

    Adicionado aplicação no ambito de teste para utilizar o h2

---
+++ src/test/java/br/com/smiles/AplicacaoLocal.java
@@ -0,0 +1,13 @@
+package br.com.smiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoLocal {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoLocal.class, args);
+	}
+
+}
